05c17b026fc6d6aff80ca6f2b41c34ac08cc4803
Fatima Mansha <fatimamansha@outlook.com>
2025-11-08 19:47:54 -0500

    fixed a syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./Header";
+import Header from "./header";
 import SearchBar from "./SearchBar";
 import RecipeList from "./RecipeList";
 import RecipeDetails from "./RecipeDetails";
